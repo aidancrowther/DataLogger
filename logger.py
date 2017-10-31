833b65d4ef14b41c77b6a619d01f139fb67c7a9b
Aidan Crowther <aidan.crowther@outlook.com>
2017-10-31 17:40:36 +0000

    Added code comments

--- logger.py
+++ logger.py
@@ -7,10 +7,12 @@ import argparse;
 import sys;
 import os;
 
+#Setup arguments for runtime
 parser = argparse.ArgumentParser();
 parser.add_argument('-v', action='store_true');
 parser.add_argument('-c');
 
+#Process arguments for logging, and permissions location
 args = parser.parse_args();
 verbose = args.v;
 if(args.c): filename = args.c;
@@ -18,54 +20,66 @@ else: filename = 'certs.txt';
 
 if(verbose): print('Starting modules...');
 
+#Start pigpio and piVirtualWire
 pi = pigpio.pi();
 rx = piVirtualWire.rx(pi, 3, 1000);
 
+#Initialize database credential variables
 dbuser='';
 dbpass='';
 dbname='';
 dbhost='';
 table='';
 
+#Look for and parse permissions
 if(os.path.exists(filename)):
 	file = open(filename);
 	certs = file.read().split('\n');
 	for cert in certs:
 		if(cert != ''): globals()[cert.split("=")[0]] = cert.split("=")[1];
 
+#Exit if no permissions are located
 else:
 	print("No cert file found, please specify a file with -c or create a certs.txt file");
 	sys.exit();
 
 if(verbose): print('Done!');
 
+#Convert the received message from an array of ASCII values to a string
 def convertMessage(message):
 
 	result = [];
 
+	#Convert each character back to ASCII text
 	for char in message:
 		result.append(chr(char));
 
+	#Join characters into a string (comma separated)
 	result = ''.join(map(str, result));
 	return result;
 
+#Split the message into an array of numeric values from the logger
 def processResult(message):
 
 	result = '';
+	#Trim out the node identifier 'N1' and generate the array
 	message = message.split(': ');
 	if(message[0] == 'N1'): result = message[1].split('\x00')[0].split(',');
 	return result;
 
+#Send received data to the database server
 def sendToDB(values):
 
 	connected = False;
 
 	if(verbose): print('Connecting to DataBase...');
+	#Attempt to connect using the provided permissions
 	try:
 		db = MySQLdb.connect(dbhost, dbuser, dbpass, dbname);
 		cursor = db.cursor();
 		connected = True;
 
+	#Report a failed attempt to connect
 	except MySQLdb.OperationalError:
 		if(verbose): print('Unable to connect, aborting write!');
 
@@ -73,20 +87,25 @@ def sendToDB(values):
 
 		if(verbose): print('Connected!\nWriting data...');
 
+		#Insert the received values into the sql query and write it to the database
 		sql = "insert into %s (temperature, humidex, pressure, light, battery) values (%s, %s, %s, %s, %s)" % (table, values[0], values[1], values[2], values[3], values[4]);
 		cursor.execute(sql);
 
 		if(verbose): print('Done writing!');
 
+		#Commit changes to the database and close the connection
 		db.commit();
 		db.close();
 
 if(verbose): print('Waiting for data...');
 
+#Main running loop
 while True:
 
+	#Wait for a message from the RF receiver
 	while rx.ready():
 
+		#Convert the received message and upload it to the database
 		received_message = convertMessage(rx.get());
 		result = processResult(received_message);
 		if(verbose): print(result);
@@ -94,5 +113,6 @@ while True:
 		sendToDB(result);
 		if(verbose): print('\nWaiting for data...');
 
+#Close piVirtualWire and pigpio
 rx.cancel();
 pi.stop();
--- transmitter.ino
+++ transmitter.ino
@@ -20,6 +20,7 @@ void setup(){
   CLKPR = (1 << CLKPCE); // enable a change to CLKPR
   CLKPR = 0; // set the CLKDIV to 0 - was 0011b = div by 8 taking 16MHz to 2MHz
 
+  //Setup pin to control power to transmitter and sensors
   pinMode(PowerPin, OUTPUT);
   digitalWrite(PowerPin, 0);
 
@@ -48,21 +49,26 @@ void setup(){
 
 void loop(){
 
+  //Turn on sensors and transmitter
   digitalWrite(PowerPin, 0);
   
+  //measure battery level and light level using the adc
   int batteryLevel = map(vccRead(), 115, 160, 0, 100);
   int lightLevel = map(analogRead(A1), 0, 350, 0, 100);
 
+  //Get data from BMP sensor
   sensors_event_t event;
   bmp.getEvent(&event);
 
   float temperature;
   bmp.getTemperature(&temperature);
 
+  //Get data from DHT sensor
   int chk = DHT.read11(DHT11_PIN);
   char msg[30];
   String message;
 
+  //Once pressure data has arrived, prepare the message, identifying the logger by a node name 'N1'
   if(event.pressure){
     message = "N1: "
     + String((int) temperature, DEC) + "," 
@@ -76,6 +82,7 @@ void loop(){
     //Serial.println("Sensor Error");
   }
 
+  //specify message length for sending
   int msgLength = message.length()+1;
 
   //Serial.println(message);
@@ -90,6 +97,7 @@ void loop(){
   sleepForMinutes(10);
 }
 
+//Go into low power mode and sleep for a specified number of minutes
 void sleepForMinutes(int i){
   i = 7*i;
   for(int time=0; time<=i; time++){
@@ -97,6 +105,7 @@ void sleepForMinutes(int i){
   }
 }
 
+//Read Vcc voltage by comparing against internal 1.1V band gap
 static byte vccRead(){
   set_sleep_mode(SLEEP_MODE_ADC);
   ADMUX = bit(REFS0) | 14;
